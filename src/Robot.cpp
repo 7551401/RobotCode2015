1f97ff50739a6c7a342e4abb5a033308f2f4814a
EMallmann <ericmallmann@gmail.com>
2015-01-15 19:34:34 -0600

    Throttle Works

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -7,10 +7,19 @@ private:
 	RobotDrive *myRobot;
 	Joystick *stick;
 	Joystick *RotStick;
+
+	Victor *vic1;
+	Victor *vic2;
+	Victor *vic3;
+	Victor *vic4;
 	void RobotInit()
 	{
 		lw = LiveWindow::GetInstance();
-		myRobot= new RobotDrive(3,2,1,0);/*possibly make two objects:
+		vic1 = new Victor(3);
+		vic2= new Victor(2);
+		vic3= new Victor(1);
+		vic4= new Victor(0);
+		myRobot= new RobotDrive(vic1,vic2,vic3,vic4);/*possibly make two objects:
 		a left and right robot for each analog stick on an x-box controller?
 		This would allow for the sticks to be used in opposite directions and
 		be able to avoid a GetTwist() method.
@@ -28,8 +37,7 @@ private:
 		myRobot->SetInvertedMotor(RobotDrive::kFrontRightMotor, true);
 		myRobot->SetInvertedMotor(RobotDrive::kRearRightMotor, true);
 		//stick= new Joystick(0);
-		stick = new Joystick (0,Joystick::kDefaultYAxis,0);
-		RotStick= new Joystick (0,Joystick::kDefaultTwistAxis, 0);
+		stick = new Joystick(0);
 
 
 
@@ -52,8 +60,32 @@ private:
 
 	void TeleopPeriodic()
 	{
-		myRobot->ArcadeDrive(stick, Joystick::kDefaultYAxis, RotStick, Joystick::kDefaultTwistAxis, true );
+		//myRobot->ArcadeDrive(stick, Joystick::kDefaultYAxis, stick, Joystick::kDefaultTwistAxis, true);
+		//myRobot->Drive(ThroStick->GetThrottle(), ThroStick->GetThrottle());
+		//vic1->Set((((ThroStick->GetThrottle()+1)*.4)+.2),0);
+		//vic2->Set((((ThroStick->GetThrottle()+1)*.4)+.2),0);
+		//vic3->Set((((ThroStick->GetThrottle()+1)*.4)+.2),0);
+		//vic4->Set((((ThroStick->GetThrottle()+1)*.4)+.2),0);
 
+
+		//float throttle = (((stick->GetThrottle()+1)*0.4)+.2);
+		//float magnitude=stick->GetMagnitude();
+		/*if (magnitude<0.08){
+			magnitude=0;*/
+		//}
+		//float direction= RotStick->GetTwist();
+		//if (direction<.2 && direction>-.2){
+			//direction=0;
+		//}
+		//direction*=.75;
+		float throttle = stick->GetThrottle();
+		throttle = ((throttle+1) *.25) + .5;
+		myRobot->ArcadeDrive(stick->GetY()*throttle, stick->GetTwist()*throttle, true);
+		//SmartDashboard::PutNumber("Mag:", magnitude);
+		//SmartDashboard::PutNumber("Dir:", direction);
+		//SmartDashboard::PutNumber("RotStick->DirectionDegrees", RotStick->GetTwist());
+		//SmartDashboard::PutNumber("stick->DirectionDegrees", stick->GetDirectionDegrees());
+		//SmartDashboard::PutNumber("RotStick->GetMag",RotStick->GetMagnitude());
 	}
 
 	void TestPeriodic()
