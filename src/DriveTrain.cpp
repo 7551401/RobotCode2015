2d086f9cf6dcdb3d51def10c28af338bd49b24b8
petertorze <peter.torzewski@charter.net>
2015-01-31 12:50:28 -0600

    Added DriveTrain.cpp and DriveTrain.h
    I broke up the drive train code and put it into two different files so
    that it is more accessible and easy to change.

---
+++ src/DriveTrain.cpp
@@ -0,0 +1,39 @@
+#include "WPILib.h"
+#include "DriveTrain.h"
+
+DriveTrain::DriveTrain() {
+	vic1 = new Victor(3);
+	vic2= new Victor(2);
+	vic3= new Victor(1);
+	vic4= new Victor(0);
+	myRobot= new RobotDrive(vic1,vic2,vic3,vic4);
+	stick = new Joystick(0);
+
+	//Motors are inverted because the motors were initially messed up
+	myRobot->SetInvertedMotor(RobotDrive::kFrontLeftMotor, true);
+	myRobot->SetInvertedMotor(RobotDrive::kRearLeftMotor, true);
+	myRobot->SetInvertedMotor(RobotDrive::kFrontRightMotor, true);
+	myRobot->SetInvertedMotor(RobotDrive::kRearRightMotor, true);
+
+};
+
+double DriveTrain::getThrottle(double val){
+	 float throttle = stick->GetThrottle();
+	 throttle++;
+	 throttle = throttle *((1-val)/2);
+	 throttle+= val;
+	 return throttle;
+
+	}
+
+//Driving in Teleop
+void DriveTrain::DriveOriented() {
+	//Scales the Joystick values by the throttle value
+	float throttle = getThrottle(.5);
+	myRobot->ArcadeDrive(stick->GetY()*throttle, stick->GetTwist()*throttle, true);
+};
+
+//Driving in Autonomous
+void DriveTrain::DriveAuto() {
+	myRobot->ArcadeDrive(1.0,0.0,true);
+}
--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,4 +1,5 @@
 #include "WPILib.h"
+#include "DriveTrain.h"
 
 
 class Robot: public IterativeRobot
@@ -6,14 +7,11 @@ class Robot: public IterativeRobot
 private:
 	Encoder *encoder;
 	LiveWindow *lw; //references LiveWindow class
-	RobotDrive *myRobot; //references RobotDrive class
+
 	Joystick *stick; //references Joystick class; stick used for forward and backward movement
 	Joystick *RotStick; //references Joystick class; RotStick used for twisting the stick
 	DoubleSolenoid *Sol; //references DoubleSolenoid class
-	Victor *vic1;
-	Victor *vic2;
-	Victor *vic3;
-	Victor *vic4;
+	DriveTrain * Drive;
 	Compressor *comp;
 	JoystickButton *button1;
 	JoystickButton *button2;
@@ -25,22 +23,12 @@ private:
 	//AxisCamera *camera;
 	//Image *frame;
 	AnalogInput *input;
-	double getThrottle(double val){
-	 float throttle = stick->GetThrottle();
-	 throttle++;
-	 throttle = throttle *((1-val)/2);
-	 throttle+= val;
-	 return throttle;
 
-	}
 	void RobotInit()
 	{
 		lw = LiveWindow::GetInstance();
-		vic1 = new Victor(3);
-		vic2= new Victor(2);
-		vic3= new Victor(1);
-		vic4= new Victor(0);
-		myRobot= new RobotDrive(vic1,vic2,vic3,vic4);
+		Drive = new DriveTrain();
+
 		std::string str= "192.168.0.90";
 		//camera= new AxisCamera(str);
 		//frame= imaqCreateImage(IMAQ_IMAGE_RGB,0);
@@ -51,11 +39,8 @@ private:
 				CameraServer::GetInstance()->StartAutomaticCapture("cam0");
 
 
-		//Motors are inverted because the motors were initially messed up
-		myRobot->SetInvertedMotor(RobotDrive::kFrontLeftMotor, true);
-		myRobot->SetInvertedMotor(RobotDrive::kRearLeftMotor, true);
-		myRobot->SetInvertedMotor(RobotDrive::kFrontRightMotor, true);
-		myRobot->SetInvertedMotor(RobotDrive::kRearRightMotor, true);
+
+
 		//stick= new Joystick(0);
 		stick = new Joystick(0); //Creates a new Joystick for forward and backward movement
 		comp= new Compressor(); //Creates a new Compressor to Compress air
@@ -94,7 +79,7 @@ private:
 			total=0;
 				}
 		if ((dis)<=0.5)	{
-			myRobot->ArcadeDrive(1.0,0.0,true);
+			Drive->DriveAuto();
 		}
 	}
 
@@ -106,19 +91,20 @@ private:
 	void TeleopPeriodic()
 	{
 
-
-		float throttle = getThrottle(.5);
-		myRobot->ArcadeDrive(stick->GetY()*throttle, stick->GetTwist()*throttle, true); //Scales the Joystick values by the throttle value
 		SmartDashboard::PutNumber("Total Distance", encoder->GetDistance());
 		SmartDashboard::PutNumber("Distance per Second", encoder->GetRate());
 		//SmartDashboard::PutNumber("Color Value: ", camera->GetColorLevel());
 		//SmartDashboard::PutNumber("Brightness: ", camera->GetBrightness());
+
+		//Solenoid Code
 		if (stick->GetRawButton(5)) {
 			Sol->Set(DoubleSolenoid::kForward);
 		}
 		if (stick->GetRawButton(6)) {
 					Sol->Set(DoubleSolenoid::kReverse);
 				}
+
+
 		//camera->GetImage(frame);
 		//imaqDrawShapeOnImage(frame,frame, {10,10,100,100}, DrawMode::IMAQ_DRAW_VALUE, ShapeMode::IMAQ_SHAPE_OVAL,0.0f);
 		//CameraServer::GetInstance()->SetImage(frame);
