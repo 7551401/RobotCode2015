46ec20b971c61ddff438f2b14f4ef5df6a5ea186
Videogameman11 <caleb.vanl@yahoo.com>
2015-02-09 19:32:28 -0600

    Add back in DriveTrain .h and .cpp

---
+++ src/DriveTrain.h
@@ -0,0 +1,28 @@
+/*
+ * DriveTrain.h
+ *
+ *  Created on: Jan 31, 2015
+ *      Author: Peter
+ */
+
+#ifndef _DRIVETRAIN_H_
+#define _DRIVETRAIN_H_
+
+class DriveTrain {
+	Victor *vic1;
+	Victor *vic2;
+	Victor *vic3;
+	Victor *vic4;
+	Joystick *stick;
+	RobotDrive *myRobot;
+
+
+public:
+	DriveTrain();
+	void DriveOriented();
+	void DriveAuto();
+	double getThrottle(double);
+	void DriveSet(float, float);
+};
+
+#endif
